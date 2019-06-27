769fbc8de9d1cba7d145b160a85f1b4bd84d3b0a
Park Ye Won <yewon0303@gmail.com>
2019-06-27 15:08:13 +0800

    improved addItemVC with switches and payer switches

--- unitravelsprac/AddItemVC.swift
+++ unitravelsprac/AddItemVC.swift
@@ -16,19 +16,52 @@ class AddItemVC: UIViewController {
     @IBOutlet weak var numberOfPeople: UITextField!
     @IBOutlet weak var pricePerPerson: UILabel!
     
+    @IBOutlet weak var tripper1Switch: UISwitch!
+    @IBOutlet weak var tripper2Switch: UISwitch!
+    @IBOutlet weak var tripper3Switch: UISwitch!
+    @IBOutlet weak var tripper4Switch: UISwitch!
+    
+    @IBOutlet weak var payer1: UISwitch!
+    @IBOutlet weak var payer2: UISwitch!
+    @IBOutlet weak var payer3: UISwitch!
+    @IBOutlet weak var payer4: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        payer1.setOn(false, animated: true)
+        payer2.setOn(false, animated: true)
+        payer3.setOn(false, animated: true)
+        payer4.setOn(false, animated: true)
 
-        // Do any additional setup after loading the view.
     }
     
     //MARK: Actions
-
+    
     @IBAction func perPerson(_ sender: Any) {
-        if let itemPrice =  Double(price.text!),
-        let n = Double(numberOfPeople.text!) {
-            let perPax:Double = itemPrice / n
+        //Double totPaid is the total amount of item to be added to Payer database
+        var totPaid: Double = Double(price.text!)
+        
+        var num: Int = 0
+        
+        if tripper1Switch.isOn {
+            num += 1
+        }
+        if tripper2Switch.isOn {
+            num += 1
+        }
+        if tripper3Switch.isOn {
+            num += 1
+        }
+        if tripper4Switch.isOn {
+            num += 1
+        }
+        
+        numberOfPeople.text = "\(num)"
+        
+        if let itemPrice =  Double(price.text!) {
+            //Double perPax is the amount of money to be paid by each person sharing
+            let perPax:Double = itemPrice / Double (num)
             pricePerPerson.text = "\(perPax)"
         } else {
             print("Please enter all details.")
