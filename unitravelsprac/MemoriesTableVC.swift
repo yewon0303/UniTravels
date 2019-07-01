49385da79ddb5660e4316db9c7ec05379945f3b8
har <harshitatiyari99@gmail.com>
2019-07-01 17:19:54 +0800

    added header to memories tab

--- unitravelsprac/MemoriesTableVC.swift
+++ unitravelsprac/MemoriesTableVC.swift
@@ -12,6 +12,8 @@ class MemoriesTableVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
