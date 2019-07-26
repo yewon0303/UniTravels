46895ed5557cbfd27e1496b0750669564cd532df
har <harshitatiyari99@gmail.com>
2019-07-26 23:37:11 +0800

    added new requests tab vc

---
+++ unitravelsprac/RequestTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequestTVCell.swift
+//  unitravelsprac
+//
+//  Created by Tiyari Harshita on 26/7/19.
+//  Copyright © 2019 Tiyari Harshita. All rights reserved.
+//
+
+import UIKit
+
+class RequestTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ unitravelsprac/RequestsVC.swift
@@ -0,0 +1,38 @@
+//
+//  RequestsVC.swift
+//  unitravelsprac
+//
+//  Created by Tiyari Harshita on 26/7/19.
+//  Copyright © 2019 Tiyari Harshita. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class RequestsVC: UIViewController {
+    
+    //MARK: Properties
+    var db: Firestore!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Actions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
